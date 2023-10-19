1affe5fc2a9024ec11f9f90dedf4c88f7f2ed826
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-10-19 17:47:29 +0530

    final commit

--- index.js
+++ index.js
@@ -22,28 +22,46 @@
 // };
 
 
-const form = document.querySelector("#searchForm");
-const imageContainer = document.querySelector("#imageContainer"); // grab the container
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#searchForm");
 
-form.addEventListener("submit", async function (e) {
-  e.preventDefault();
-  const searchTerm = form.elements.query.value;
-  const config = { params: { q: searchTerm } };
-  const res = await axios.get(`https://api.tvmaze.com/search/shows?`, config);
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-  // Clear existing images
-  imageContainer.innerHTML = "";
-
-  makeImages(res.data);
-  form.elements.query.value = "";
+    const searchTerm = e.target.elements.query.value;
+    if (searchTerm) {
+      try {
+        const shows = await fetchShows(searchTerm);
+        displayShows(shows);
+      } catch (err) {
+        console.error("Error fetching shows:", err);
+        console.log("oh nooo");
+        // Optionally display an error message to the user
+      }
+      e.target.elements.query.value = "";
+    }
+  });
 });
 
-const makeImages = (shows) => {
-  for (let result of shows) {
-    if (result.show.image) {
+const fetchShows = async (query) => {
+  const res = await axios.get("https://api.tvmaze.com/search/shows", {
+    params: { q: query },
+  });
+  return res.data;
+};
+
+const displayShows = (shows) => {
+  const imageContainer = document.querySelector("#imageContainer");
+  imageContainer.innerHTML = "";
+
+  for (let {
+    show: { image },
+  } of shows) {
+    if (image && image.medium) {
       const img = document.createElement("IMG");
-      img.src = result.show.image.medium;
-      imageContainer.append(img); // append images to the container
+      img.src = image.medium;
+      imageContainer.append(img);
     }
   }
 };
+
